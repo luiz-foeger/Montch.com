aa42583f10e9d7db72132869d4755043eb174440
Luiz Felipe Föeger <luizffsan5@gmail.com>
2025-04-25 15:45:21 -0300

    Criação do componente Header
    Criação e estilização do componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Route, Routes } from 'react-router-dom'; // import dos componentes da biblioteca de rotas 
 
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header'; // import do componente Header
+// import Home from './pages/Home'; // import do componente Home
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
       </Routes>
     </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import { RiArrowDownSLine } from "react-icons/ri";
+
+import estilos from './Header.module.css';
+
+function Header() {
+    return (
+        <header className={estilos.header}>
+            <img className={estilos.logo} src="/img/logo.png" alt="Logo" />
+            <nav className={estilos.nav}>
+                <h3>
+                    <Link to="/">Início</Link>
+
+                    <div className={estilos.opcaoDropdown}>
+                        <Link to="#">Produtos <RiArrowDownSLine /></Link>
+                        <ul className={estilos.dropdownProdutos}>
+                            <li><Link to="/conjuntos" style={{ marginTop: "20px" }}>Conjuntos</Link></li>
+                            <li><Link to="/shorts">Shorts</Link></li>
+                            <li><Link to="/camisetas">Camisetas</Link></li>
+                            <li><Link to="/bermudas">Bermudas</Link></li>
+                        </ul>
+                    </div>
+
+                    <Link to="/sobre-nos">Sobre nós</Link>
+                </h3>
+            </nav>
+            <img className={estilos.logo} src="/img/logo.png" alt="Logo" />
+        </header>
+    );
+}
+
+
+export default Header;
