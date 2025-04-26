d5a6d26fe24448e9ca9d7f0e62e064bf468d3e8f
Luiz Felipe Föeger <luizffsan5@gmail.com>
2025-04-26 00:07:55 -0300

    Alterações no modal de detalhes
    Alterações no modal de detalhes do produto

--- src/components/ListaProdutos/ListaProdutos.jsx
+++ src/components/ListaProdutos/ListaProdutos.jsx
@@ -3,22 +3,59 @@ import ProdutoCard from '../ProdutoCard/ProdutoCard';
 
 function ListaProdutos() {
     const produtos = [
-        { id: 1, imagem: '/assets/images/produtos/1.png', nome: 'Vanguard', preco: '44,90' },
-        { id: 2, imagem: '/assets/images/produtos/2.png', nome: 'Impulso', preco: '179,90' },
-        { id: 3, imagem: '/assets/images/produtos/3.png', nome: 'Força Dupla', preco: '139,90' },
-        { id: 3, imagem: '/assets/images/produtos/4.png', nome: 'Vibe', preco: '69,90' },
-        { id: 4, imagem: '/assets/images/produtos/5.png', nome: 'Fogo&Vento', preco: '139,90' },
-        { id: 4, imagem: '/assets/images/produtos/6.png', nome: 'Essência', preco: '44,90' },
+        {
+            id: 1,
+            imagem: '/assets/images/produtos/1.png',
+            nome: 'Vanguard',
+            preco: '44,90',
+            descricao: 'Desafie o comum, seja extraordinário!',
+        },
+        {
+            id: 2,
+            imagem: '/assets/images/produtos/6.png',
+            nome: 'Essência',
+            preco: '44,90',
+            descricao: 'A verdadeira força está no movimento!',
+        },
+        {
+            id: 3,
+            imagem: '/assets/images/produtos/2.png',
+            nome: 'Impulso',
+            preco: '179,90',
+            descricao: 'Energia que veste, potência que impulsiona!',
+        },
+        {
+            id: 3,
+            imagem: '/assets/images/produtos/3.png',
+            nome: 'Força Dupla',
+            preco: '139,90',
+            descricao: 'Dois lados e um só propósito: potência e determinação em cada movimento!',
+        },
+        {
+            id: 4,
+            imagem: '/assets/images/produtos/4.png',
+            nome: 'Vibe',
+            preco: '69,90',
+            descricao: 'Acelere seus limites, movimente-se com intensidade!',
+        },
+        {
+            id: 5,
+            imagem: '/assets/images/produtos/5.png',
+            nome: 'Fogo&Vento',
+            preco: '139,90',
+            descricao: 'Coragem que queima, leveza que voa!',
+        },
     ];
 
     return (
         <div className={estilos.lista}>
             {produtos.map(produto => (
                 <ProdutoCard
-                    key={produto.id}
+                    id={produto.id}
                     imagem={produto.imagem}
                     nome={produto.nome}
                     preco={produto.preco}
+                    descricao={produto.descricao}
                 />
             ))}
         </div>
--- src/components/ProdutoCard/ProdutoCard.jsx
+++ src/components/ProdutoCard/ProdutoCard.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import estilos from './ProdutoCard.module.css';
 import { FaWhatsapp } from 'react-icons/fa';
 
+// import do icone de fechar modal
+import { FaTimes } from 'react-icons/fa';
+
 function ProdutoCard({ imagem, nome, preco, descricao }) {
     const [mostrarModal, setMostrarModal] = useState(false);
 
@@ -12,7 +15,7 @@ function ProdutoCard({ imagem, nome, preco, descricao }) {
                 <h2 className={estilos.nome}>{nome}</h2>
                 <p className={estilos.preco}>R$ {preco}</p>
                 <a href={`https://api.whatsapp.com/send?phone=5527996180495&text=Oi, vim pelo site e gostaria de saber mais sobre o produto ${nome}! 🚀`} target="_blank" rel="noopener noreferrer" className={estilos.botaoContato}>
-                    Saiba mais <FaWhatsapp className={estilos.icone} />
+                    Comprar <FaWhatsapp className={estilos.icone} />
                 </a>
             </div>
 
@@ -22,10 +25,13 @@ function ProdutoCard({ imagem, nome, preco, descricao }) {
                         <img src={imagem} alt={nome} className={estilos.modalImagem} />
                         <div>
                             <h2>{nome}</h2>
-                            <p>{descricao}</p>
+                            <p className={estilos.descricao}>{descricao}</p>
+                            {/* <p className={estilos.hashtag}>#VaiComTudo</p> */}
                             <p className={estilos.preco}>R$ {preco}</p>
-                            <p>#VaiComTudo</p>
-                            <button onClick={() => setMostrarModal(false)}>X</button>
+                            <a href={`https://api.whatsapp.com/send?phone=5527996180495&text=Oi, vim pelo site e gostaria de saber mais sobre o produto ${nome}! 🚀`} target="_blank" rel="noopener noreferrer" className={estilos.botaoContato}>
+                                Comprar <FaWhatsapp className={estilos.icone} />
+                            </a>
+                            <button className={estilos.botaoFechar} onClick={() => setMostrarModal(false)}><FaTimes /></button>
                         </div>
                     </div>
                 </div>
